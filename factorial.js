116ed63b46a80082a75eafd04aec4a218a8457fc
Surbhi Sakhiya <surbhisakhiya28@gmail.com>
2025-12-29 20:46:34 +0530

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,12 @@
+// find Factorial Number
+
+let n= Number(prompt("Enter the Number"))
+let fact = 1;
+
+for(let i = 1;i<=n;i++)
+{
+    fact = fact *i
+}
+
+console.log("Factorial number " + fact)
+
---
+++ table.js
@@ -0,0 +1,6 @@
+// print any table
+let num = Number(prompt("Enter the Number"))
+for(let i =0;i<=10;i++)
+{
+    console.log(num + "*" + i + "=" + (num*i))
+}
